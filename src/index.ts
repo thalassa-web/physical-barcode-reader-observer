3055d99983884650ccaab02be20b240fc4e59d04
Bruno Desprez <bruno.desprez@gmail.com>
2019-01-16 23:53:44 +0100

    [+] Comments

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,27 @@ import {Observable} from 'rxjs';
 import {bufferToggle, filter, map} from 'rxjs/operators';
 
 export class PhysicalBarcodeReaderObserver {
+    /**
+     * The usable observable to subscribe to
+     */
+    public onBarcodeRead$: Observable<string>;
+    /**
+     * Keypress event on document to Observable
+     */
     private readonly onKeypress$: Observable<KeyboardEvent> = Observable.fromEvent(document, 'keypress');
+    /**
+     * The enter key pressed event
+     */
     private readonly onEnter$: Observable<KeyboardEvent> = this.onKeypress$.pipe(filter(ev => ev.key === 'Enter'));
-    private readonly onStart$: Observable<KeyboardEvent>;
-    public onBarcodeRead$: Observable<string> = this.onKeypress$.pipe(
-        bufferToggle(this.onStart$, () => this.onEnter$),
-        map(events => events.filter(ev => ev.key.length === 1).reduce((acc, cur) => acc + cur.key, '')),
-    );
-
+    /**
+     * Getting reader with specific prefix
+     * @param prefix
+     */
     constructor(prefix?: string) {
-        this.onStart$ = this.onKeypress$.pipe(filter(ev => !prefix || ev.key === prefix));
+        const onStart$ = this.onKeypress$.pipe(filter(ev => !prefix || ev.key === prefix));
+        this.onBarcodeRead$ = this.onKeypress$.pipe(
+            bufferToggle(onStart$, () => this.onEnter$),
+            map(events => events.filter(ev => ev.key.length === 1).reduce((acc, cur) => acc + cur.key, '')),
+        );
     }
 }
