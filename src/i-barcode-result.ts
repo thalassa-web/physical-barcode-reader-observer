f5a06f7d8008375d19b125dd76cd189ce6af0088
Bruno Desprez <bruno.desprez@gmail.com>
2019-01-17 23:52:25 +0100

    [*] Code simplification [+] Use a timer to know if the barcode is finally read [TODO] Replace index.ts by events.ts

--- src/i-barcode-result.ts
+++ src/i-barcode-result.ts
@@ -7,3 +7,15 @@ export interface IBarcodeResult {
   barcode: string;
   type: BarcodeType;
 }
+
+/**
+ * Transform array of keys into a IBarcodeResult
+ * @param keys
+ */
+export const keysToBarcodeResult = (keys: string[]) => {
+    const result: IBarcodeResult = { barcode: keys.join(''), type: BarcodeType.UNKNOWN };
+    if (/\d{13}/.test(result.barcode)) {
+        result.type = BarcodeType.EAN_13;
+    }
+    return result;
+};
