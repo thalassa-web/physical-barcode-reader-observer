0ab94d02178c6d986ce52633ce2a490677d8d2cf
Bruno Desprez <bdesprez@thalassa.fr>
2019-01-18 17:27:05 +0100

    2.0.4

--- src/__tests__/barcode-type-detector.test.ts
+++ src/__tests__/barcode-type-detector.test.ts
@@ -1,18 +1,18 @@
-import {BarcodeTypeDetector} from "../barcode-type-detector";
-import {BarcodeType} from "../enums";
+import { BarcodeTypeDetector } from '../barcode-type-detector';
+import { BarcodeType } from '../enums';
 
 test('EAN_13', () => {
-    expect(BarcodeTypeDetector.detect('3614950268099')).toBe(BarcodeType.EAN_13);
+  expect(BarcodeTypeDetector.detect('3614950268099')).toBe(BarcodeType.EAN_13);
 });
 
 test('EAN_8', () => {
-    expect(BarcodeTypeDetector.detect('78635708')).toBe(BarcodeType.EAN_8);
+  expect(BarcodeTypeDetector.detect('78635708')).toBe(BarcodeType.EAN_8);
 });
 
 test('UPC_A', () => {
-    expect(BarcodeTypeDetector.detect('012345678905')).toBe(BarcodeType.UPC_A);
+  expect(BarcodeTypeDetector.detect('012345678905')).toBe(BarcodeType.UPC_A);
 });
 
 test('UPC_E', () => {
-    expect(BarcodeTypeDetector.detect('8635709')).toBe(BarcodeType.UPC_E);
+  expect(BarcodeTypeDetector.detect('8635709')).toBe(BarcodeType.UPC_E);
 });
